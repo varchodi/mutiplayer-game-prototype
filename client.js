ff27807ace57db3e52c018c97bf44274e07835d8
varchodi <rauschenrobert@gmail.com>
2024-07-13 17:59:39 +0300

    rmv js files

--- client.js
+++ client.js
@@ -4,8 +4,13 @@
     const ws = new WebSocket("ws://localhost:6970");
     // LISTENERS 
     ws.addEventListener("close", (event) => console.log("WEBSOCKET CLOSE,", event));
-    ws.addEventListener("error", (event) => console.log(`WEBSOCKET ERROR, ${event}`));
-    ws.addEventListener("message", (event) => console.log(`WEBSOCKET MESSAGE, ${event}`));
+    ws.addEventListener("error", (event) => {
+        console.log(`WEBSOCKET ERROR, ${event}`);
+    });
+    ws.addEventListener("message", (event) => {
+        const message = JSON.parse(event.data);
+        console.log(`WEBSOCKET MESSAGE, ${message}`);
+    });
     ws.addEventListener("open", (event) => console.log(`WEBSOCKET OPEN, ${event}`));
 })();
 console.log("hello bro cool");
--- common.ts
+++ common.ts
@@ -1,3 +1,8 @@
 export const SERVER_PORT = 6970;
 export const WORLD_WIDTH = 800;
 export const WORLD_HEIGHT = 600;
+
+export interface Hello{
+    kind: "Hello",
+    id:number,
+}
